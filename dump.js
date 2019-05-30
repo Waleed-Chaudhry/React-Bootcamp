13f80a8f5e492c3cb1daea68765817c9aab592aa
Waleed Chaudhry <waleed.u.chaudhry@gmail.com>
2019-05-30 18:13:12 -0400

    Create dump.js

---
+++ dump.js
@@ -0,0 +1,37 @@
+// Radium Code:
+import Radium, { StyleRoot} from 'radium' // For using media queries
+
+// Using in line styles
+// Not use normally, but if you only want to change the button in this class and not all the buttons
+const style = {
+  backgroundColor: 'green', // default color
+  color: 'white',
+  font: 'inherit',
+  border: '1px solid blue',
+  padding: '8px',
+  cursor: 'pointer',
+  // Setting hover after adding Radium to the app
+  ':hover': {
+    backgroundColor: 'lightgreen',
+    color: 'black'
+  }
+}
+
+style.backgroundColor= 'red' //Changing style conditionally
+Changing the value of :hover using a string, not a dot
+style[':hover'] = {
+  backgroundColor: 'salmon',
+  color: 'black'  
+}
+
+export default Radium(App); // Radium wraps around your app
+
+import Radium from 'radium'
+
+const style = {
+  '@media (min-width: 500px)' : {
+    width: '450px'
+  }
+}
+
+export default Radium(person);
